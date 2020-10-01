20f6691867ecf8668d0a35c3b1f354c4ba017504
leejaehoon43 <ljw160791@gmail.com>
2020-10-01 21:42:55 +0900

    게시판 조회 기능 구현, 글 수정 및 삭제, 게시판 다듬기, 벨리데이션 체크, 페이징 구현, 검색 기능 구현, 페이징 검색 유지 기능 추가

--- src/main/java/kr/co/dao/BoardDAOImpl.java
+++ src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -1,11 +1,11 @@
 package kr.co.dao;
 
 import java.util.List;
-
 import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 import kr.co.vo.BoardVO;
+import kr.co.vo.SearchCriteria;
 
 @Repository
 public class BoardDAOImpl implements BoardDAO{
@@ -19,7 +19,27 @@ public class BoardDAOImpl implements BoardDAO{
 	}
 
 	@Override
-	public List<BoardVO> list() throws Exception {
-		return sqlSession.selectList("boardMapper.list");
+	public List<BoardVO> list(SearchCriteria scri) throws Exception {
+		return sqlSession.selectList("boardMapper.listPage", scri);
+	}
+	
+	@Override
+	public int listCount(SearchCriteria scri) throws Exception {
+		return sqlSession.selectOne("boardMapper.listCount", scri);
+	}
+
+	@Override
+	public BoardVO read(int bno) throws Exception {
+		return sqlSession.selectOne("boardMapper.read", bno);
+	}
+
+	@Override
+	public void update(BoardVO boardVO) throws Exception {
+		sqlSession.update("boardMapper.update", boardVO);
+	}
+
+	@Override
+	public void delete(int bno) throws Exception {
+		sqlSession.delete("boardMapper.delete", bno);
 	}
 }
--- src/main/java/kr/co/service/BoardServiceImpl.java
+++ src/main/java/kr/co/service/BoardServiceImpl.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 import kr.co.dao.BoardDAO;
 import kr.co.vo.BoardVO;
+import kr.co.vo.SearchCriteria;
 
 @Service
 public class BoardServiceImpl implements BoardService{
@@ -18,7 +19,27 @@ public class BoardServiceImpl implements BoardService{
 	}
 
 	@Override
-	public List<BoardVO> list() throws Exception {
-		return dao.list();
+	public List<BoardVO> list(SearchCriteria scri) throws Exception {
+		return dao.list(scri);
+	}
+	
+	@Override
+	public int listCount(SearchCriteria scri) throws Exception {
+		return dao.listCount(scri);
+	}
+
+	@Override
+	public BoardVO read(int bno) throws Exception {
+		return dao.read(bno);
+	}
+
+	@Override
+	public void update(BoardVO boardVO) throws Exception {
+		dao.update(boardVO);
+	}
+
+	@Override
+	public void delete(int bno) throws Exception {
+		dao.delete(bno);
 	}
 }
